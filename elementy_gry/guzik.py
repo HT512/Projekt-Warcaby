6402cf45397934dd0a6b98eb75b38e1f58d64434
damia <damia@DESKTOP-CH161HL>
2022-01-12 02:01:57 +0100

    Dodaj pliki projektów.

---
+++ elementy_gry/guzik.py
@@ -0,0 +1,258 @@
+from gui import kolory
+
+class Guzik:
+        
+    """reprezentacja guzika"""
+    def __init__(self, x, y, colour, dlugosc_boku1, dlugosc_boku2):
+            
+        #wymiary
+        self.bok_1=dlugosc_boku1
+        self.bok_2=dlugosc_boku2
+        
+        #umiejscowienie na ekranie
+        
+        #pomocnicza lambda
+        mnozenie = lambda x, y : x * y
+        
+        self.x_ekranu = mnozenie(x,self.bok_1)
+        self.y_ekranu = mnozenie(y,self.bok_2)
+        
+        
+        #kolor
+        self.colour_aktualny = colour
+        self.colour_staly = colour
+        
+       
+           #get   
+                   
+    def get_x_ekranu(self):
+        """zwraca pozycje x ekranu"""
+        return self.x_ekranu
+    
+    def get_y_ekranu(self):
+        """zwraca pozycje y ekranu"""
+        return self.y_ekranu
+    
+    def get_wiersz_planszy(self):
+        """zwraca wiersz planszy na którym jest guzik"""
+        return self.wiersz_planszy
+    
+    def get_kolumna_planszy(self):
+        """zwraca kolumnê planszy na której jest guzik"""
+        return self.kolumna_planszy  
+
+    def pobierz_bok_1(self):
+        """zwraca dlugosc boku1 guzika"""
+        return self.bok_1
+    
+    def pobierz_bok_2(self):
+        """zwraca dlugosc boku2 guzika"""
+        return self.bok_2
+        
+    def get_podswietlony(self):
+        """zwraca informacjê o tym, czy guzik jest aktualnie podœwietlony"""
+        return self.podswietlony
+     
+    def sprawdz_czy_najechany(self, mouse_width, mouse_height):
+        """sprwadza czy guzik jest aktualnie najechany myszk¹"""
+        #pomocnicza lambda
+        dodawanie = lambda x, y : x + y
+        
+        if (self.x_ekranu < mouse_width < dodawanie(self.x_ekranu,self.bok_1)) and (self.y_ekranu < mouse_height < dodawanie(self.y_ekranu,self.bok_2)) :
+
+            return True
+        else:
+            return False
+            
+    
+    #dla GUI
+    
+    def draw_button(self, display):
+        """maluje guzik"""
+        display.maluj_guzik(self.colour_aktualny, self.x_ekranu, self.y_ekranu, self.bok_1, self.bok_2)
+        
+        
+    def podswietl_button(self, display):
+        """podœwietla guzik"""
+        self.colour_aktualny = kolory.color_light
+        self.draw_button(display)
+            
+    def zgas_button(self, display):
+        """gasi guzik"""
+        self.colour_aktualny = self.colour_staly
+        self.draw_button(display)
+
+    
+    #wirtualne
+    
+    def get_pionek(self):
+        raise NotImplementedError
+        
+    def get_pusty(self):
+        raise NotImplementedError
+        
+    def ustaw_czy_pusty(Self):
+        raise NotImplementedError
+            
+            
+ #///////////////////////////////////////////////////////////////////////////           
+            
+            
+
+class Guzik_planszy(Guzik):
+    """guzik planszy, a wiêc pole planszy"""
+    def __init__(self, x, y, colour, dlugosc_boku1, dlugosc_boku2, wiersz_planszy):
+        super().__init__(x, y, colour, dlugosc_boku1, dlugosc_boku2)
+        
+        #umiejscowie na planszy
+        self.wiersz_planszy = wiersz_planszy
+        self.kolumna_planszy = x
+        
+        self.pusty = True
+        self.pionek = 0
+        
+        #set
+        
+    def ustaw_czy_pusty(self, value, pionek):
+        """ustawia, czy pole jest puste, czy zajête przez dany pionek"""
+        self.pusty = value
+        self.pionek = pionek
+        
+        
+        # get
+        
+    def get_pionek(self):
+        """zwraca pionek znajduj¹cy siê na guziku"""
+        return self.pionek
+    
+    def get_pusty(self):
+        """zwraca informacjê, czy na polu stoi pionek, czy nie"""
+        return self.pusty
+    
+    
+    # ewentualne metody, gdyby by³y potrzebne do reprezentacji w konsoli
+    def __str__(self): 
+        return 'obiekt button, wiersz_planszy = {}, kolumna_planszy={}'.format(self.wiersz_planszy, self.kolumna_planszy)
+    
+    def __repr__(self): 
+        return 'BUTTON([{}][{}])'.format(self.wiersz_planszy, self.kolumna_planszy)
+    
+    # dla GUI
+    
+    #nadpisujemy metode z klasy bazowej
+    def podswietl_button(self, display):
+        """podœwietla guzik"""
+        Guzik.podswietl_button(self, display)
+            
+        #ustawiamy pionek
+        if(self.pusty != True):
+            self.pionek.maluj_pionek(display) 
+        
+        
+       #nadopisujemy metode z klasy bazowej 
+    def zgas_button(self, display):
+        """gasi guzik"""
+        Guzik.zgas_button(self, display)
+            
+            #ustawiamy pionek
+        if(self.pusty != True):
+            self.pionek.maluj_pionek(display) 
+                
+                
+                
+#//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////                
+                
+    
+      
+class Reset_button(Guzik):
+    """guzik odpowiedzialny za resetowanie gry"""
+    def __init__(self, width_ekranu, height_ekranu):      
+    
+        self.x_ekranu = width_ekranu - (1/3*height_ekranu)
+        self.y_ekranu = (1+3.5)*(height_ekranu/10)       #tam gdzie zaczyna sie 4 wiersz planszy 1 to tam gdzie se zaczyna plansza
+        
+        #dlugosci wymiary guzika
+        self.bok_2 = (height_ekranu/10)  #jak zwykly guzik 
+        self.bok_1 =  1/3*2/3*height_ekranu   # taka sama 
+        
+        self.colour_staly = kolory.color_white
+        self.colour_aktualny = kolory.color_white
+        
+        self.napis = 'RESET'
+        
+     
+
+    #dla GUI    
+        
+    def podswietl_button(self, display):
+        """podswietla guzik"""
+        Guzik.podswietl_button(self, display)
+        display.wstaw_napis_na_guziku(self.napis, self.x_ekranu, self.y_ekranu, kolory.color_black)
+            
+    def zgas_button(self, display):
+        """gasi guzik"""
+        Guzik.zgas_button(self, display)
+        display.wstaw_napis_na_guziku(self.napis, self.x_ekranu, self.y_ekranu, kolory.color_black)
+            
+
+    def draw_reset_button(self, display):
+        """maluje reset guzik"""
+        Guzik.draw_button(self, display)
+        display.wstaw_napis_na_guziku(self.napis, self.x_ekranu, self.y_ekranu, kolory.color_black)
+   
+        
+        
+       
+        
+        
+        
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
+        
+        
+        
+class okey(Guzik):
+    """guzik okey"""
+    def __init__(self, width_ekranu, height_ekranu, gracz):
+              
+        self.gracz = gracz
+        
+        if self.gracz.get_kolor_gracza() == kolory.color_white :
+            
+            self.x_ekranu = width_ekranu - (1/3*height_ekranu) - (height_ekranu/10)
+            self.y_ekranu = 7*(height_ekranu/10)+100        #tam gdzie zaczyna sie 4 wiersz planszy 1 to tam gdzie se zaczyna plansza
+        
+        else:
+            self.x_ekranu = width_ekranu - (1/3*height_ekranu) - (height_ekranu/10)
+            self.y_ekranu = 1*(height_ekranu/10)+100          
+        
+                
+        #dlugosci wymiary guzika
+        self.bok_2 = 80
+        self.bok_1 =  190
+        
+        self.colour_staly = kolory.color_yellow
+        self.colour_aktualny = kolory.color_yellow
+        
+        self.napis = 'OKEY'
+        
+     
+   
+    #dla GUI    
+        
+    def podswietl_button(self, display):
+        """podswietla guzik"""
+        Guzik.podswietl_button(self, display)
+        display.wstaw_napis_na_guziku(self.napis, self.x_ekranu, self.y_ekranu, kolory.color_black) 
+            
+    def zgas_button(self, display):
+        """gasi guzik"""
+        Guzik.zgas_button(self, display)
+        display.wstaw_napis_na_guziku(self.napis, self.x_ekranu, self.y_ekranu, kolory.color_black) 
+        
+    def draw_okey_button(self, display):
+        """maluje guzik okey"""
+        Guzik.draw_button(self, display)
+        display.wstaw_napis_na_guziku(self.napis, self.x_ekranu, self.y_ekranu, kolory.color_black)    
+
+
+
---
+++ szachownica.py
@@ -0,0 +1,115 @@
+﻿
+from elementy_gry import guzik
+from interfejs import kolory
+
+class Szachownica:
+    """reprezentuje szachownice"""
+    def __init__(self):
+        
+        #wymiary
+        self.liczba_wierszy_planszy = 8                
+        self.liczba_kolumn_planszy = 8
+            
+        #początek szachownicy na ekranie
+        self.start_wiersz_ekranu = 1
+            
+        #siatka guzików
+        self.net_buttons = []
+        
+        #rozmiar pola (guzika)
+        self.rozmiar_pola = 0
+      
+    
+        #get
+            
+    def get_net_buttons(self):
+        """zwraca siatkę przycisków"""
+        return self.net_buttons
+    
+    def get_ilosc_wierszy(self):
+        """zwraca ilość wierszy, z których zbudowana jest szachownica"""
+        return self.liczba_wierszy_planszy
+    
+    def get_ilosc_kolumn(self):
+        """zwraca ilość kolumn, z których zbudowana jest szachownica"""
+        return self.liczba_kolumn_planszy
+    
+     
+        #set
+            
+    def ustaw_rozmiar_pola(self, width_ekranu, height_ekranu):
+        """ustawia rozmiar pola"""
+        self.rozmiar_pola = height_ekranu/10   #dzieki temu wysokosc ekranu podzielona na 10 kawalkow i zaczynamy od wiersza o indeksie 1 
+            
+            
+            
+            
+        #inne metody   
+            
+    def create_net_buttons(self):   # 8x8  buttons[8][8]
+        """tworzy siatke przycisków, a więc szachownice"""
+        net = [[0] * self.liczba_kolumn_planszy for i in range(self.liczba_wierszy_planszy)]
+        return net
+    
+ 
+        #set
+
+    def set_net_buttons(self):
+        """ustawia guziki"""
+        self.net_buttons = self.create_net_buttons()
+            
+        #malujemy 64 guzików, czyli tworzymy plasze 8x8
+            
+        wiersz_planszy = -1  #od 0 indexowane
+    
+        for wiersz_ekranu in range (self.start_wiersz_ekranu, self.start_wiersz_ekranu+self.liczba_wierszy_planszy):
+            wiersz_planszy += 1
+            for kolumna in range (0,self.liczba_kolumn_planszy):
+                    
+                #ustawiamy kolor guzika co drugi ciemny na przemian z białym
+                
+                f = wynik_modulo(2)
+                if(f(wiersz_planszy)==0 and f(kolumna)==0 or f(wiersz_planszy)!=0 and f(kolumna)!=0):
+                    button_colour = kolory.color_white
+                else:
+                    button_colour = kolory.color_dark
+
+
+                object_guzik = guzik.Guzik_planszy(kolumna, wiersz_ekranu, button_colour, self.rozmiar_pola, self.rozmiar_pola, wiersz_planszy) 
+            
+                #wypelniamy siatkę stworzonym guzikiem
+                self.net_buttons[wiersz_planszy][kolumna]= object_guzik
+            
+       
+        
+  
+
+           
+    #FUNKCJA TWORZĄCA SZACHOWNICE
+    
+def create_szachownica(display):
+    """tworzy szachownice do gry"""
+    szachownica = Szachownica()
+    width_ekranu, height_ekranu = display.get_okno_pygame().get_size() 
+    szachownica.ustaw_rozmiar_pola(width_ekranu, height_ekranu)
+    szachownica.set_net_buttons()
+        
+    return szachownica
+        
+       
+        
+        #lambda do uproszczenia obliczeń modulo
+def wynik_modulo(liczba):
+    """dla uproszczenia wykonuje modulo"""
+    return lambda val: (val % liczba)
+
+
+    
+    #generator dla stworzenia szeregu przycisków z siatki przycisków
+def wygeneruj_net_jako_szereg(net_buttons):
+    """generator, który z siatki przycisków tworzy szereg guzików"""
+    for wiersz_guzikow in net_buttons:
+        for guzik in wiersz_guzikow:
+            yield guzik              # yield oznacza, że jest to generator - nie zwykła funkcja
+
+               
